6195d97a505a2068fed287e6e34c54e16f51386f
Isaac Lobo <lobo.isaac92@gmail.com>
2022-09-28 17:09:49 -0300

    add facebook authenticantion feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,13 @@
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors'
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+namespace FacebookAuthentication{
+  export type Params = {
+    token: string
+  }
+  export type Result = AccessToken | AuthenticationError
+}
